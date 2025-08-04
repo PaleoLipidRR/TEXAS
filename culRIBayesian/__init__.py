54995bee6825a0b8f38a976c572f8e69b45ce26a
Ronnakrit "Ronnie" Rattanasriampaipong <55888172+PaleoLipidRR@users.noreply.github.com>
2025-08-04 07:32:01 -0700

    update

--- culRIBayesian/__init__.py
+++
@@ -1,7 +0,0 @@
-import warnings
-warnings.warn(
-    "The `culRIBayesian` package name is deprecated; please use `TEXAS` instead.",
-    DeprecationWarning,
-    stacklevel=2,
-)
-from TEXAS import *
